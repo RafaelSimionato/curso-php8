3a035d54a6f4c2e5367207e5892eb09f5701ee32
Rafael Simionato <simionatoscript@gmail.com>
2022-03-23 17:45:26 +0100

    Added aula 09
    Aula 09 adicionada

---
+++ aula09/1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <!-- Comentário em HTML -->
+    <p>Teste </p>
+
+    // Comentário inválido de PHP
+
+    <?php
+
+    //Comentário válido de PHP
+    //Comentários em PHP não aparecem no código fonte da página, porém, de HTML aparecem
+    echo "PHP";
+
+    ?>
+
+</body>
+</html>
---
+++ aula09/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// COMENTÁRIOS
+// =================================================================
+
+// Comentário de uma linha
+
+# Comentário de uma linha
+
+/* Comentário em 
+multiplas linhas
+*/
+
+echo 'Exemplo de comentários ';
+// echo '<br> ';
+# echo 'Ignorar esta instrução com um comentário ';
