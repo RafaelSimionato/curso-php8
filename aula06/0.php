c4c951bac9b388e91247ab30b6a0f40eec25acb5
Rafael Simionato <101069896+RafaelSimionato@users.noreply.github.com>
2022-03-24 20:45:32 +0100

    Update 0.php
    Just word corrections

--- aula06/0.php
+++ aula06/0.php
@@ -2,4 +2,4 @@
 
 //Codigo de PHP
 
-/*Esta e a maneira tradicional de cração, e não é necessario fechar a tag PHP no final, basta ter somente a tag de abertura e depois os codigos (Isto quando não existe codigo HTML junto) */
+/*Esta e a maneira tradicional de criação, e não é necessario fechar a tag PHP no final, basta ter somente a tag de abertura e depois os codigos (Isto quando não existe codigo HTML junto) */
