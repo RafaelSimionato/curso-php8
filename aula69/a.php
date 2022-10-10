443556ab2d3d9a3bf1c922b39df85bc150712418
Rafael Simionato <simionatoscript@gmail.com>
2022-10-10 02:09:26 +0200

    Added aula 69

---
+++ aula69/a.php
@@ -0,0 +1,47 @@
+<?php
+
+# O PRÓXIMO PASSO... 
+
+/*
+Ao longo desta playlist estivemos a ver muitos aspetos sobre
+a sintaxe da linguagem
+
+Na playlist em sequência, vamos começar a ver o PHP em ação
+
+Vamos usar os conhecimentos de escrita de sintaxe, aplicados a 
+exemplos práticos de pequena dimensão
+
+Iremos acrescentar mais alguns conceitos relacionados com a sintaxe,
+mas em ambiente de desenvolvimento de exemplos
+
+Entre outras materias, vamos estudar:
+
+> Técnicas para iniciar e estruturar um projeto
+> Explorar a utilização de funções para strings
+> Explorar a utilização de funções para arrays
+> Manipulação de Datas
+    DateTime, formatação de datas e operações com datas
+> Manipulação de arquivos
+    Criação de ficheiros, leitura de dados, ficheiros CSV
+    Criação de pastas
+
+> Formulários e filtros
+> Upload de ficheiros
+> Sessões
+> Cookies
+> Tratamento de erros
+> PHP para criação de processos na linha de comandos
+> Acesso a banco de dados PDO
+> Construção de APIs simples
+> Envio de emails
+> Composer e dependências
+> Criptografia
+> Interação entre PHP e JavaScript
+> Padrões de programação
+> Padrão MVC
+> Introdução da Frameworks
+> Hospedagem / alojamento de projetos em webserver
+
+e muito mais... 
+
+*/
